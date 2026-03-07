f4f88ac5c64c01df2b69a48aff8489a3f8e20325
Akkadecht <jayjamesnew4@gmail.com>
2026-03-07 12:14:14 +0700

    update auth login jwt

--- backend/app/auth.py
+++ backend/app/auth.py
@@ -4,6 +4,27 @@ from sqlalchemy.orm import Session
 from app.model import User
 from app.schemas import UserResponseRegister,UserCreateRegister,UserCreateLogin
 from app.database import get_db
+from jose import jwt
+from datetime import datetime, timedelta
+import os
+
+
+SECRET_KEY = os.getenv("SECRET_KEY")
+ALGORITHM = "HS256"
+ACCESS_TOKEN_EXPIRE_MINUTES = 60
+
+def create_access_token(data:dict):
+    to_encode = data.copy()
+    expire = datetime.utcnow() + timedelta(minutes=ACCESS_TOKEN_EXPIRE_MINUTES)
+    to_encode.update({"exp": expire})
+    encoded_jwt = jwt.encode(to_encode, SECRET_KEY, algorithm=ALGORITHM)
+    return encoded_jwt
+
+
+
+
+
+
 
 router = APIRouter(prefix="/auth",tags=["Auth"])
 
@@ -42,4 +63,15 @@ def login(user:UserCreateLogin,db:Session =  Depends(get_db)):
     db.add(db_user)
     db.commit()
     db.refresh(db_user)
-    return {"message":"ล็อกอินสำเร็จ","id":db_user.id,"role":db_user.rolestatus}
+    access_token = create_access_token(
+        data={
+            "sub": db_user.username,
+            "user_id": db_user.id,
+            "role": db_user.rolestatus
+        }
+    )
+    return{
+        "access_token": access_token,
+        "token_type": "bearer",
+        "role": db_user.rolestatus
+        }
